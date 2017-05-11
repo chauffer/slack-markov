20811aae1387d8ff155abd563385eaf7d9ab8188
Simone Esposito <chaufnet@gmail.com>
2017-05-11 20:23:31 +0200

    fix bot id

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@ import logging.config
 import os
 
 SLACK_API_TOKEN = os.environ['MARKOV_SLACK_API_TOKEN']
-SLACK_BOT_ID = os.getenv('MARKOV_SLACK_BOT_ID', 'B2C3QDCUR')
+SLACK_BOT_ID = os.getenv('MARKOV_SLACK_BOT_ID', 'U2C3DJYAJ')
 
 SLACK_CHANNEL_MAP = os.getenv('MARKOV_SLACK_CHANNEL_MAP', 'devs-off-topic:devs-on-topic').split(',') # "original:fake,original:fake"
 ENGINE_CYCLE_SLEEP = int(os.getenv('MARKOV_ENGINE_CYCLE_SLEEP', '1'))
