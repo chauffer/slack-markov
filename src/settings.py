91f2ab34fc2a2daf60af18f0c96048dfcd1e7f70
Ondřej Garncarz <ondrej@garncarz.cz>
2017-05-11 20:17:31 +0200

    Logging adjusted.

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,4 @@
+import logging.config
 import os
 
 SLACK_API_TOKEN = os.environ['MARKOV_SLACK_API_TOKEN']
@@ -15,3 +16,36 @@ for channel_map in SLACK_CHANNEL_MAP:
     channels_original.append(c_map[0])
     channels_fake.append(c_map[1])
     channels.append([c_map[0], c_map[1]])
+
+
+LOGGING = lambda: {
+    'version': 1,
+    'disable_existing_loggers': True,
+
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s][%(levelname)s] %(name)s '
+                      '%(filename)s:%(funcName)s:%(lineno)d | %(message)s',
+        },
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
+
+
+LOGGING = LOGGING()
+logging.config.dictConfig(LOGGING)
