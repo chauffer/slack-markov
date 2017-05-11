1228f413c3e2824ae9ba2939766f555632404632
Simone Esposito <chaufnet@gmail.com>
2017-05-11 19:40:39 +0200

    fix syntafix syntaxx

--- src/settings.py
+++ src/settings.py
@@ -4,10 +4,11 @@ SLACK_API_TOKEN = os.environ['MARKOV_SLACK_API_TOKEN']
 
 SLACK_CHANNEL_MAP = os.getenv('MARKOV_SLACK_CHANNEL_MAP', 'devs-off-topic:devs-on-topic').split(',') # "original:fake,original:fake"
 ENGINE_CYCLE_SLEEP = int(os.getenv('MARKOV_ENGINE_CYCLE_SLEEP', '1'))
+
 channels, channels_original, channels_fake = [], [], []
 
 for channel_map in SLACK_CHANNEL_MAP:
-    c_map = SLACK_CHANNEL_MAP.split(':')
+    c_map = channel_map.split(':')
 
     channels_original.append(c_map[0])
     channels_fake.append(c_map[1])
